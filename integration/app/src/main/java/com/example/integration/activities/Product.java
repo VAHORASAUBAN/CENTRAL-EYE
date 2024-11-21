971e545438e98a6926c5ff918f63874ce6ef699d
VAHORASAUBAN <sauban.imscit21@gmail.com>
2024-11-22 01:14:08 +0530

    Adaptor

---
+++ integration/app/src/main/java/com/example/integration/activities/Product.java
@@ -0,0 +1,25 @@
+package com.example.integration.activities;
+
+public class Product {
+    private String name;
+    private String barcode;
+    private String id;
+
+    public Product(String name, String barcode, String id) {
+        this.name = name;
+        this.barcode = barcode;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
