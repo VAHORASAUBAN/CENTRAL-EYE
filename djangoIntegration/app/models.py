6846a0def4cf68e4f4d2c292bce118fb796043bb
its-ayanshaikh <ayan.imscit21@gmail.com>
2024-11-26 11:41:55 +0530

    database upgraded

--- djangoIntegration/app/models.py
+++ djangoIntegration/app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import timedelta
 
 class stationDetails(models.Model):
     station_id = models.IntegerField(primary_key=True)
@@ -38,12 +39,20 @@ class AssetCategory(models.Model):
     def __str__(self):
         return self.category_name
     
+class AssetSubCategory(models.Model):
+    sub_category_id = models.IntegerField(primary_key=True)
+    category = models.ForeignKey(AssetCategory, null=True, blank=True, on_delete=models.SET_NULL)
+    sub_category_name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.sub_category_name
+    
 
 class Asset(models.Model):
     asset_id = models.IntegerField(primary_key=True, unique=True)
     asset_name = models.CharField(max_length=255)
+    asset_category = models.ForeignKey(AssetSubCategory, null=True, blank=True, on_delete=models.SET_NULL)
     barcode = models.CharField(max_length=255, unique=True)
-    asset_type = models.CharField(max_length=255)
     purchase_date = models.DateField()
     asset_value = models.CharField(max_length=255)
     condition = models.CharField(max_length=255)
@@ -64,3 +73,29 @@ class Allocation(models.Model):
     def __str__(self):
         return f"Asset : {self.asset_barcode} - Allocated to: {self.user}"
     
+class RequestAsset(models.Model):
+    request_id = models.IntegerField(primary_key=True, unique=True)
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
+    asset = models.ForeignKey(Asset, null=True, blank=True, on_delete=models.SET_NULL)
+    quantity = models.CharField(max_length=255)
+    request_date = models.DateField(auto_now_add=True)
+    request_location = models.CharField(max_length=255)
+    status = models.CharField(max_length=255, default='Pending')
+    
+    def __str__(self):
+        return f"Request : {self.request_id} - by: {self.user}"
+
+class Maintenance(models.Model):
+    maintenance_id = models.AutoField(primary_key=True)
+    asset = models.ForeignKey("Asset", null=True, blank=True, on_delete=models.SET_NULL)
+    last_maintenance_date = models.DateField()  # Tracks the last maintenance date
+    next_maintenance_date = models.DateField()  # Tracks the next maintenance date
+
+    def save(self, *args, **kwargs):
+        if not self.next_maintenance_date and self.last_maintenance_date:
+            # Calculate the next maintenance date based on last maintenance date
+            self.next_maintenance_date = self.last_maintenance_date + timedelta(days=180)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Maintenance for Asset: {self.asset} - Next Maintenance: {self.next_maintenance_date}"
